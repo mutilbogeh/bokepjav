5cfa16ecbd4d32f6cd43bbba0d5a7ec597451828
mutilbogeh <mutilbogeh@gmail.com>
2024-05-22 07:33:01 +0700

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# bokepjav
-bokepjav
+A live demo of the project is available
---
+++ app/layout.tsx
@@ -0,0 +1,99 @@
+import "./globals.css";
+import Script from "next/script";
+import type { Metadata } from "next";
+import { Nunito } from "next/font/google";
+import { SITENAME } from "@/lib/constants";
+import { ThemeProvider } from "@/components/theme-provider";
+
+const font = Nunito({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+    title: `${SITENAME} - Video Bokep Indo Viral Terbaru`,
+    description: `${SITENAME} Video Bokep Indo Jepang Jav Barat Simontok Viral Terbaru Bocil Ngentot Jilbab Smp Mama Sma korea china tante live paksa ngentot abg cewek pelajar.`,
+    metadataBase: new URL("https://bokepjav.click"),
+    alternates: {
+        canonical: `/`,
+    },
+    openGraph: {
+        title: `${SITENAME} - Video Bokep Indo Jepang Barat Viral Terbaru`,
+        description: `${SITENAME} Video Bokep Indo Jepang Jav Barat Simontok Viral Terbaru Bocil Ngentot Jilbab Smp Mama Sma korea china tante live paksa ngentot abg cewek pelajar.`,
+        url: `/`,
+        type: `website`,
+    },
+    verification: {
+        google: 'OmKX2QOAp7DkhFfH4rQWll0fuLtzBY5XsYrKlp0ssas',
+        yandex: 'b4f8b16346e212a8',
+    },
+};
+
+export const runtime = "edge";
+
+export default function RootLayout({
+    children,
+}: {
+    children: React.ReactNode;
+}) {
+    const jsonLd = {
+        "@context": "https://schema.org",
+        "@type": "Organization",
+        "name": `${SITENAME}`,
+        "description": `${SITENAME} Video Bokep Indo Jepang Jav Barat Simontok Viral Terbaru Bocil Ngentot Jilbab Smp Mama Sma korea china tante live paksa ngentot abg cewek pelajar.`,
+        "logo": "https://bokepjav.click/favicon.ico",
+        "url": "https://bokepjav.click",
+        "founder":{
+            "@type":"Person",
+            "name":"admin",
+            "url":"https://bokepjav.click"
+            },
+            "foundingDate":"2024-04-09"
+        }
+        const jsonLd1 = {
+            "@context": "https://schema.org",
+        "@type": "WebPage",
+        "name": `${SITENAME}`,
+        "url": "https://bokepjav.click",
+        "description": `${SITENAME} Video Bokep Indo Jepang Jav Barat Simontok Viral Terbaru Bocil Ngentot Jilbab Smp Mama Sma korea china tante live paksa ngentot abg cewek pelajar.`,
+        "image": "https://bokepjav.click/favicon.ico",
+        "potentialAction":{
+            "@type":"ReadAction",
+            "target":"https://bokepjav.click"}
+        }
+        const jsonLd2 = {
+            "@context": "https://schema.org",
+        "@type": "WebSite",
+        "name": `${SITENAME}`,
+        "url": "https://bokepjav.click",
+        "description": `${SITENAME} Video Bokep Indo Jepang Jav Barat Simontok Viral Terbaru Bocil Ngentot Jilbab Smp Mama Sma korea china tante live paksa ngentot abg cewek pelajar.`,
+        "potentialAction": { 
+        "@type": "SearchAction", 
+          "target": "https://bokepjav.click/?q={search_term}", 
+            "query-input": "required name=search_term"}
+        }
+    return (
+        <html lang="en" suppressHydrationWarning>
+            <body className={font.className}>
+                <ThemeProvider
+                    attribute="class"
+                    defaultTheme="system"
+                    enableSystem
+                    disableTransitionOnChange
+                >
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
+        />
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd1) }}
+        />
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd2) }}
+        />
+                    {children}
+                </ThemeProvider>
+            </body>
+<Script src="https://js.juicyads.com/jp.php?c=947403z2v256s2x2w264x294&u=http%3A%2F%2Fwww.juicyads.rocks"/>
+        </html>
+    );
+}
